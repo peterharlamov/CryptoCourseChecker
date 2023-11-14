d522720654054bd7765751b71a0ea999ac51b777
Peter <pjotrharlamov99@gmail.com>
2023-11-15 01:08:38 +0200

    added new features

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import requests;
 print("- Crypto Checking Application -");
 
 while True:
-    crypto = input("Enter the name of crypto (bitcoin): ").lower();
+    repeats = int(input("Select a count of cypto tokens you want to check: "))
+    all_crypto_tokens = []
+    for i in range(repeats):
+        crypto = input("Enter the name of crypto (bitcoin): ").lower();
+        all_crypto_tokens.append(crypto);
     currency = input("Enter the currency: ").lower();
 
     def get_crypto_price(coin):
@@ -18,11 +22,12 @@ while True:
         else:
             return None;
     
-    price = get_crypto_price(crypto);
-    if price is not None:
-        print(f"Current course {crypto.capitalize()}: {price} {currency.upper()}");
-    else:
-        print("Sorry, you crypto is not find of not available!");
+    for i in range(repeats):
+        price = get_crypto_price(all_crypto_tokens[i]);
+        if price is not None:
+            print(f"Current course {all_crypto_tokens[i].capitalize()}: {price} {currency.upper()}");
+        else:
+            print("Sorry, you crypto is not find of not available!");
     exit = input("Enter anything to exit: ");
     if exit.isalpha() or exit.isdigit() is True:
         break;
